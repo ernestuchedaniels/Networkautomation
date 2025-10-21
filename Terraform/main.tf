09ca8e4dd26d5a003982bab5d73ea096b065c01c
Ernest Daniels <ernestdaniels@mac.mynetworksettings.com>
2025-10-21 14:54:35 -0400

    Added management network to OSPF

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -33,6 +33,23 @@ resource "iosxe_interface_ethernet" "r2gi1" {
   ipv4_address      = "192.168.1.2"
   ipv4_address_mask = "255.255.255.0"
 }
+resource "iosxe_interface_ethernet" "r1lo0" {
+  device            = "iosxe-router-1"
+  type              = "Loopback"
+  name              = "0"
+  shutdown          = false
+  ipv4_address      = "10.151.0.1"
+  ipv4_address_mask = "255.255.255.255"
+}
+
+resource "iosxe_interface_ethernet" "r2lo0" {
+  device            = "iosxe-router-2"
+  type              = "Loopback"
+  name              = "0"
+  shutdown          = false
+  ipv4_address      = "10.10.0.2"
+  ipv4_address_mask = "255.255.255.255"
+}
 resource "iosxe_ospf" "ospf1" {
   for_each   = toset(var.routers)
   device     = each.key
@@ -49,4 +66,30 @@ resource "iosxe_ospf" "ospf1" {
       area     = "0"
     }
   ]
+}
+
+resource "iosxe_ospf" "ospfnetr1" {
+  device     = iosxe-router-1
+  process_id = 1
+  networks = [
+    {
+      ip       = "10.151.0.0"
+      wildcard = "0.0.0.255"
+      area     = "0"
+    },
+  ]
+  depends_on = [r1lo0]
+}
+
+resource "iosxe_ospf" "ospfnetr2" {
+  device     = iosxe-router-2
+  process_id = 1
+  networks = [
+    {
+      ip       = "10.10.0.0"
+      wildcard = "0.0.0.255"
+      area     = "0"
+    },
+  ]
+  depends_on = [r2lo0]
 }
